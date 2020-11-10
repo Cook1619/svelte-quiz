953d31cfe127066e27a4ce0b27e617b29c2b1e64
Cook1619 <cook6348@gmail.com>
2020-11-10 10:03:16 -0600

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let name;
+	import Quiz from './Quiz.svelte';
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<Quiz quizName="New Quiz"/>
+</main>
+
+<style>
+	h1 {
+		color: red;
+	}
+</style>
